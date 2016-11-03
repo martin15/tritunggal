8a96eec6675f9a30a7325bf65c6b0a536d7ba505
Martin <martin.me15@yahoo.com>
2016-11-03 23:43:51 +0700

    added default user

--- tritunggal/db/seeds.rb
+++ tritunggal/db/seeds.rb
@@ -9,4 +9,10 @@
 about_us = SystemSetting.find_or_create_by(:name => "About Us")
 puts about_us.inspect
 
-
+user  = User.find_by_email("martin.me15@yahoo.com")
+if user.nil?
+  user_type_5 = User.create( :email => 'martin.me15@yahoo.com', :password => "12345678",
+                             :password_confirmation => "12345678",
+                             :first_name => "Martin", :last_name => "Martin",
+                             :confirmed_at => Time.now)
+end
