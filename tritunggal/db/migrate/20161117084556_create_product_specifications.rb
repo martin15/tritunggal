64100b26eaa818c060cf25b1ab322ffe55d05d34
Martin <martin.me15@yahoo.com>
2016-11-21 16:47:40 +0700

    added missing table

--- tritunggal/db/migrate/20161117084556_create_product_specifications.rb
+++ tritunggal/db/migrate/20161117084556_create_product_specifications.rb
@@ -2,6 +2,7 @@ class CreateProductSpecifications < ActiveRecord::Migration
   def change
     create_table :product_specifications do |t|
       t.string  :filename
+      t.string  :file_type
       t.integer :product_id
       t.timestamps null: false
     end
